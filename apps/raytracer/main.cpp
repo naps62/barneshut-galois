743317704e3fc20c789c840e2a9f8c69ec6124f3
Pedro Costa <dev@pfac.info>
2012-07-17 16:47:13 -0500

    main.cpp cleaned

--- apps/raytracer/main.cpp
+++ apps/raytracer/main.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt 
 #include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2 
 
+// Usage: time ./smallpt 5000 && xv image.ppm
+
 /***********************************************************
  * DATA
  */
@@ -96,29 +98,38 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi){
 
 
 
-
+// Usage: time ./smallpt 5000 && xv image.ppm
 int main(int argc, char *argv[]){ 
-	int w=1024, h=768, samps = argc==2 ? atoi(argv[1])/4 : 1; // # samples 
-	Ray cam(Vec(50,52,295.6), Vec(0,-0.042612,-1).norm()); // cam pos, dir 
-	Vec cx=Vec(w*.5135/h), cy=(cx%cam.d).norm()*.5135, r, *c=new Vec[w*h]; 
+	int w = 1024;
+	int h = 768;
+	int samps = (argc == 2) ? atoi(argv[1]) / 4 : 1; // # samples 
+	Ray cam(Vec(50, 52, 295.6), Vec(0,-0.042612, -1).norm());// cam pos, dir 
+	Vec cx = Vec(w * .5135 / h);
+	Vec cy = (cx % cam.d).norm() * .5135;
+	Vec r;
+	Vec *c = new Vec[w * h];
+
 #pragma omp parallel for schedule(dynamic, 1) private(r)       // OpenMP 
-	for (int y=0; y<h; y++){                       // Loop over image rows 
-		fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1)); 
-		for (unsigned short x=0, Xi[3]={0,0,y*y*y}; x<w; x++)   // Loop cols 
-			for (int sy=0, i=(h-y-1)*w+x; sy<2; sy++)     // 2x2 subpixel rows 
-				for (int sx=0; sx<2; sx++, r=Vec()){        // 2x2 subpixel cols 
-					for (int s=0; s<samps; s++){ 
-						double r1=2*erand48(Xi), dx=r1<1 ? sqrt(r1)-1: 1-sqrt(2-r1); 
-						double r2=2*erand48(Xi), dy=r2<1 ? sqrt(r2)-1: 1-sqrt(2-r2); 
-						Vec d = cx*( ( (sx+.5 + dx)/2 + x)/w - .5) + 
-							cy*( ( (sy+.5 + dy)/2 + y)/h - .5) + cam.d; 
-						r = r + radiance(Ray(cam.o+d*140,d.norm()),0,Xi)*(1./samps); 
-					} // Camera rays are pushed ^^^^^ forward to start in interior 
-					c[i] = c[i] + Vec(clamp(r.x),clamp(r.y),clamp(r.z))*.25; 
+	for (int y = 0; y < h; y++) {                       // Loop over image rows 
+		fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h-1)); 
+		for (unsigned short x = 0, Xi[3] = {0, 0, y * y * y}; x < w; x++)   // Loop cols 
+			for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++)     // 2x2 subpixel rows 
+				for (int sx = 0; sx < 2; sx++, r = Vec()){        // 2x2 subpixel cols 
+					for (int s = 0; s < samps; s++){ 
+						double r1 = 2 * erand48(Xi);
+						double dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+						double r2 = 2 * erand48(Xi);
+						double dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2); 
+						Vec d = cx * ( ( (sx + .5 + dx) / 2 + x) / w - .5) + 
+							cy * ( ( (sy + .5 + dy) / 2 + y) / h - .5) + cam.d; 
+						r = r + radiance(Ray(cam.o + d * 140, d.norm()), 0, Xi) * (1. / samps); 
+					} // Camera rays are pushed ^^^^^ forward to start in interior
+					c[i] = c[i] + Vec(clamp(r.x), clamp(r.y) , clamp(r.z)) * .25; 
 				} 
 	} 
 	FILE *f = fopen("image.ppm", "w");         // Write image to PPM file. 
 	fprintf(f, "P3\n%d %d\n%d\n", w, h, 255); 
-	for (int i=0; i<w*h; i++) 
+	for (int i=0; i < (w * h); i++) 
 		fprintf(f,"%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z)); 
 }
+// Usage: time ./smallpt 5000 && xv image.ppm
