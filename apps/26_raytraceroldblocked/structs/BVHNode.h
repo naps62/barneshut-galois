d1308d0f5a00cee06b6c9f668a348ef8d28ce711
Miguel Palhas <mpalhas@gmail.com>
2012-07-30 12:13:26 -0500

    minor changes

---
+++ apps/26_raytraceroldblocked/structs/BVHNode.h
@@ -0,0 +1,75 @@
+#ifndef _BVHNODE_H
+#define _BVHNODE_H
+
+#include <vector>
+
+#include "BVHNode.h"
+#include "Box.h"
+
+/**
+ * BVHTree
+ */
+struct BVHNode {
+	// instance ID
+	const uint id;
+
+	// parent node
+	BVHNode * const parent;
+
+	// bouding box describing this node
+	Box box;
+
+	// is this a leaf node?
+	bool leaf;
+
+	// childs (BVHNode* if !leaf, T* otherwise)
+	union {
+		BVHNode *node;
+		Object *leaf;
+	} childs[2];
+
+	//
+	// Methods
+	//
+
+	/**
+	 * Constructors
+	 */
+	BVHNode();
+
+	BVHNode(BVHNode * const _parent);
+
+	BVHNode(	BVHNode * const _parent,
+				std::vector<Object*>& elems,
+				std::vector<Object*>& tmp_elems,
+				const uint start,
+				const uint end,
+				const uint axis);
+
+	// updates bounding box on this node, and recurses up the tree
+	void updateTreeBox(const Box& box);
+
+	bool recurseTree(const Ray& r, double& dist, Object *& obj) const;
+
+	bool intersectBlock(vector<Ray*> rays, std::vector<std::pair<double, Object*>& colisions) const;
+
+	/**
+	 * Output
+	 */
+	//friend std::ostream& operator<<(std::ostream& os, const BVHNode& p);
+	std::string toString() const;
+
+	private:
+	void sortObjects(	std::vector<Object*>& elems,
+							const uint start,
+							const uint end,
+							const uint axis);
+
+	void buildNode(std::vector<Object*>& elems,
+						std::vector<Object*>& tmp_elems,
+						uint start,
+						uint end,
+						uint axis);
+};
+
+#endif // _BVHNODE_H
---
+++ apps/26_raytraceroldblocked/structs/BVHTree.h
@@ -0,0 +1,31 @@
+#ifndef _BVHTREE_H
+#define _BVHTREE_H
+
+#include <ostream>
+#include <vector>
+
+#include "BVHNode.h"
+
+struct BVHTree {
+
+	BVHNode *root;
+
+	/**
+	 * Constructors
+	 */
+	BVHTree(std::vector<Object*>& elems);
+
+	// returns true if the Ray intersects an object of the tree, also giving the distance and object as secondary results
+	bool intersect(const Ray& r, double& dist, Object *& obj) const;
+
+	bool intersectBlock(vector<Ray*> rays, std::vector<std::pair<double, Object*>& colisions) const;
+
+	// dumps the entire tree in DOT format
+	void dumpDot(std::ostream& os) const;
+
+	private:
+	// build the tree from a given collection of objects
+	void buildTree(std::vector<Object*>& elems);
+};
+
+#endif // _BVHTREE_H
